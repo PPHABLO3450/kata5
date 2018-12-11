6ef873b16643d167384f7af0d1a14f052aacff87
wwwam <wwwam@DESKTOP-SB95MQH>
2018-12-11 17:42:59 +0000

    primera version

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -16,6 +16,7 @@ import java.sql.SQLException;
 public class Kata5 {
     
     /**
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args){ 
---
+++ src/kata5/SelectApp.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+/**
+ *
+ * @author pablo
+ */
+
+public class SelectApp {
+    // Conect to data base
+    private Connection connect() {
+        String url = "jdbc:sqlite:Kata5.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    public void selectAll(){
+        String sql = "SELECT * FROM PEOPLE";
+        try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)){
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" +
+                        rs.getString("Name") + "\t" +
+                        rs.getString("Apellidos") + "\t" +
+                        rs.getString("Departamento") + "\t");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }    
+}
